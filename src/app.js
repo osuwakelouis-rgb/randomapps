8816d9253660dff6a40a69441f787a14a2d343db
osuwakelouis-rgb <osuwakelouis@gmail.com>
2025-09-24 13:08:11 +0100

    pushing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import pastQuestionsRoutes from "./routes/pastQuestions.js";
 
 const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
